0b4027f10f6243d9d0938a38790676c0f57f184d
“parthishere” <parthishere1234@gmail.com>
2023-05-11 16:22:06 +0530

    changes

--- rfid/api/urls.py
+++ rfid/api/urls.py
@@ -5,7 +5,7 @@ from .views import ESPListCreateAPI, ESPRetriveUpdateDestroyAPIView, return_data
 app_name = "rfid-api"
 
 urlpatterns = [
-    path("", ESPListCreateAPI.as_view(), name="list-esp"),
+    path("esp-list", ESPListCreateAPI.as_view(), name="list-esp"),
     path("<str:unique_id>", ESPRetriveUpdateDestroyAPIView.as_view(), name="detail-esp"),
     
     path("write-to-rfid/", return_data_to_esp_view, name="list-rfid"),
