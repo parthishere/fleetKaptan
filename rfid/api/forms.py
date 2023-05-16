6208f42df628daf015c06bc3f2fc308c14cd7cc3
“parthishere” <parthishere1234@gmail.com>
2023-05-16 18:22:19 +0530

    wokring

--- rfid/api/forms.py
+++ rfid/api/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 
 class SaveForm(forms.Form):
-    password = forms.CharField(label='password', max_length=200)
     D0 = forms.CharField(label='D0', max_length=200)
     data = forms.CharField(label="data", max_length=200)
-    username = forms.CharField(label='username', max_length=200)
-    uqid = forms.CharField(label='uqid', max_length=200)
+
     
