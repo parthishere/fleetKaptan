97a3dd57cd2db5df3bdeb570204d933834080ff6
“parthishere” <parthishere1234@gmail.com>
2023-05-24 16:16:37 +0530

    changes

--- rfid/api/views.py
+++ rfid/api/views.py
@@ -105,7 +105,7 @@ def get_posted_data_from_esp(request, unique_id, username):
     D0 = form.cleaned_data.get('D0')
     data = form.cleaned_data.get('data')
     uid = form.cleaned_data.get('uid')
-    print(password, D0, data, uqid, username)
+    print(D0, data, uqid, username)
     try:
         esp = Esp32.objects.get(user__username=username, unique_id=uqid)
         print(esp)
